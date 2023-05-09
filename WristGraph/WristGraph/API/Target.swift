24e97668b2d320539ef57f142c6a64a464602265
Hikaru Aohara <hikaruaohara@icloud.com>
2023-05-10 02:50:25 +0900

    Added Target.swift

---
+++ WristGraph/WristGraph/API/Target.swift
@@ -0,0 +1,65 @@
+//
+//  Target.swift
+//  WristGraph
+//
+//  Created by 青原光 on 2023/05/10.
+//
+
+import Foundation
+import Moya
+
+public enum Target: Decodable {
+    case getGraph(userName: String)
+}
+
+extension Target: TargetType {
+    public var baseURL: URL {
+        return URL(string: "https://api.github.com")!
+    }
+
+    public var path: String {
+        switch self {
+        case .getGraph:
+            return "/graphql"
+        }
+    }
+
+    public var method: Moya.Method {
+        switch self {
+        case .getGraph:
+            return .post
+        }
+    }
+
+    public var task: Task {
+        switch self {
+        case .getGraph(let userName):
+            let query = """
+            query {
+              user(login: "\(userName)") {
+                contributionsCollection {
+                  contributionCalendar {
+                    weeks {
+                      contributionDays {
+                        date
+                        color
+                        contributionLevel
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """
+            return .requestParameters(parameters: ["query": query], encoding: JSONEncoding.default)
+        }
+    }
+
+    public var headers: [String: String]? {
+        switch self {
+        case .getGraph:
+            return ["Authorization": "Bearer \(accessToken)"]
+        }
+    }
+}
+
