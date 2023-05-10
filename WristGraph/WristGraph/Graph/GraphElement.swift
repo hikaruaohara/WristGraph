40114cde56824eeaf82035d667aeb6d4b999b1ae
Hikaru Aohara <hikaruaohara@icloud.com>
2023-05-10 15:22:15 +0900

    Implemented GraphElement View

--- WristGraph/WristGraph/Graph/GraphElement.swift
+++ WristGraph/WristGraph/Graph/GraphElement.swift
@@ -7,14 +7,43 @@
 
 import SwiftUI
 
+extension Color {
+    init(hex: String) {
+        let hex_ = hex.replacingOccurrences(of: "#", with: "")
+        let scanner = Scanner(string: hex_)
+        var rgbValue: UInt64 = 0
+
+        if scanner.scanHexInt64(&rgbValue) {
+            let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+            let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+            let blue = Double(rgbValue & 0x0000FF) / 255.0
+
+            self.init(red: red, green: green, blue: blue)
+            return
+        }
+
+        self.init(red: 0, green: 0, blue: 0)
+    }
+}
+
 struct GraphElement: View {
+    private let color: Color
+
+    init(color: String) {
+        self.color = Color(hex: color)
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(color)
+                .aspectRatio(1, contentMode: .fit)
+        }
     }
 }
 
 struct GraphElement_Previews: PreviewProvider {
     static var previews: some View {
-        GraphElement()
+        GraphElement(color: "#9be9a8")
     }
 }
