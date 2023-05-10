13b6749ef19c1e97c432dc83a612aee2c83982b9
Hikaru Aohara <hikaruaohara@icloud.com>
2023-05-10 14:01:33 +0900

    Created new branch Added files

---
+++ WristGraph/WristGraph/Graph/GraphElement.swift
@@ -0,0 +1,20 @@
+//
+//  GraphElement.swift
+//  WristGraph
+//
+//  Created by 青原光 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct GraphElement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphElement_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphElement()
+    }
+}
---
+++ WristGraph/WristGraph/Graph/GraphView.swift
@@ -0,0 +1,20 @@
+//
+//  GraphView.swift
+//  WristGraph
+//
+//  Created by 青原光 on 2023/05/10.
+//
+
+import SwiftUI
+
+struct GraphView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphView_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphView()
+    }
+}
