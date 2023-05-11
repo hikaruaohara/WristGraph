9ad0c38a7a39b045ef9ce1b9fcf649c8f1e9334b
Hikaru Aohara <hikaruaohara@icloud.com>
2023-05-11 23:40:10 +0900

    Changed folder structure

--- WristGraph/WristGraph Watch App/ContentView.swift
+++ WristGraph/WristGraph Watch App/ContentView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var followers = UserDefaults.standard.stringArray(forKey: "followers") ?? [String]()
+
     var body: some View {
         GeometryReader { geometry in
-            ForEach(followers, id: \.self) { follower in
-                NavigationLink(destination: GraphView(userName: follower)) {
+            List {
+                ForEach(followers, id: \.self) { follower in
                     ListItem(userName: follower)
                         .frame(height: 41 * geometry.size.width / 70)
                 }
