d159ad92e27c277d1d47c9df37eb8f815737f69d
Hikaru Aohara <hikaruaohara@icloud.com>
2023-05-16 16:44:21 +0900

    Changed complication view

--- WristGraph/WristGraphWidget Watch/WristGraphWidget_Watch.swift
+++ WristGraph/WristGraphWidget Watch/WristGraphWidget_Watch.swift
@@ -10,49 +10,71 @@ import SwiftUI
 
 struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date())
+        SimpleEntry(date: Date(), weeks: [[String()]])
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date())
+        let entry = SimpleEntry(date: Date(), weeks: [[String()]])
         completion(entry)
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        Task {
+            var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate)
-            entries.append(entry)
+            // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+            let weeks = await fetchData()
+            let date = Calendar.current.date(byAdding: .hour, value: 0, to: Date())!
+            entries.append(SimpleEntry(date: date, weeks: weeks))
+
+//            let currentDate = Date()
+//            for hourOffset in 0 ..< 5 {
+//                let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+//                let entry = SimpleEntry(date: entryDate)
+//                entries.append(entry)
+//            }
+
+            let timeline = Timeline(entries: entries, policy: .atEnd)
+            completion(timeline)
+        }
+    }
+
+    func fetchData() async -> [[String]] {
+        var weeks = Array(repeating: [String](), count: 7)
+
+        do {
+            let weeks_ = try await Request.shared.getGraph(userName: "hikaruaohara")
+            let displayedWeeks = weeks_[weeks_.count - 19 ..< weeks_.count]
+            for week in displayedWeeks {
+                for day in week.contributionDays {
+                    weeks[day.weekday].append(day.contributionLevel)
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
         }
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        return weeks
     }
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
+    var date: Date
+    let weeks: [[String]]
 }
 
 struct WristGraphWidget_WatchEntryView : View {
     var entry: Provider.Entry
-    let numOfColumns = 19
-    let levelDict = ["NONE", "FIRST_QUARTILE", "SECOND_QUARTILE", "THIRD_QUARTILE", "FOURTH_QUARTILE"]
 
     var body: some View {
         GeometryReader { geometry in
             let size = (geometry.size.height * 5 - 10) / 41
             VStack {
                 Grid(horizontalSpacing: size / 5, verticalSpacing: size / 5) {
-                    ForEach(0..<7, id: \.self) { i in
+                    ForEach(0..<entry.weeks.count, id: \.self) { i in
                         GridRow {
-                            ForEach(0..<numOfColumns, id: \.self) { j in
-                                let rand = Int.random(in: 0..<4)
-                                GraphElement(contributionLevel: levelDict[rand], size: size)
+                            ForEach(0..<entry.weeks[i].count, id: \.self) { j in
+                                GraphElement(contributionLevel: entry.weeks[i][j], size: size)
                             }
                         }
                     }
@@ -78,7 +100,7 @@ struct WristGraphWidget_Watch: Widget {
 
 struct WristGraphWidget_Watch_Previews: PreviewProvider {
     static var previews: some View {
-        WristGraphWidget_WatchEntryView(entry: SimpleEntry(date: Date()))
+        WristGraphWidget_WatchEntryView(entry: SimpleEntry(date: Date(), weeks: [[String()]]))
             .previewContext(WidgetPreviewContext(family: .accessoryRectangular))
     }
 }
