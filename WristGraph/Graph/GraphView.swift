1bf194adfb1c48fd55164f903ef9575786f7b172
Hikaru Aohara <hikaruaohara@icloud.com>
2023-05-29 23:40:34 +0900

    Applied GraphView to MVVM architecture

--- WristGraph/Graph/GraphView.swift
+++ WristGraph/Graph/GraphView.swift
@@ -9,90 +9,31 @@ import SwiftUI
 import UIKit
 
 struct GraphView: View {
-    let userName: String
-    private let numOfColumns: Int
-    private let graphFrame: (width: CGFloat, height: CGFloat)
-    @State private var weeks = Array(repeating: [String](), count: 7)
-    @State private var isLoading = false
-    @Environment(\.scenePhase) private var scenePhase
-
-    init(userName: String, numOfColumns: Int) {
-        self.userName = userName
-        self.numOfColumns = numOfColumns
-
-        #if os(iOS)
-        graphFrame.width = UIScreen.main.bounds.width
-        #elseif os(watchOS)
-        graphFrame.width = WKInterfaceDevice.current().screenBounds.width
-        #endif
-
-        graphFrame.height = graphFrame.width * 41 / (6 * CGFloat(self.numOfColumns) - 1)
-    }
+    @ObservedObject var graphViewModel: GraphViewModel
+    @EnvironmentObject private var model: Model
 
     var body: some View {
-        VStack {
-            if (isLoading) {
-                ProgressView()
-            } else {
-                let size = graphFrame.height * 5 / 41
+        VStack(alignment: .leading) {
+            Text(graphViewModel.userName)
+                .bold()
 
-                Grid(horizontalSpacing: size / 5, verticalSpacing: size / 5) {
-                    ForEach(0..<weeks.count, id: \.self) { i in
-                        GridRow {
-                            ForEach(0..<weeks[i].count, id: \.self) { j in
-                                GraphElement(contributionLevel: weeks[i][j], size: size)
-                            }
+            let size = graphViewModel.graphFrame.height * 5 / 41
+            Grid(horizontalSpacing: size / 5, verticalSpacing: size / 5) {
+                ForEach(0..<graphViewModel.weeks.count, id: \.self) { i in
+                    GridRow {
+                        ForEach(0..<graphViewModel.weeks[i].count, id: \.self) { j in
+                            GraphElement(contributionLevel: graphViewModel.weeks[i][j], size: size)
                         }
                     }
                 }
             }
         }
-        .frame(width: graphFrame.width, height: graphFrame.height)
-        .onTapGesture {
-            Task {
-                await fetchData()
-            }
-        }
-        .disabled(isLoading)
-        .onChange(of: scenePhase) { phase in
-            if phase == .active {
-                Task {
-                    await fetchData()
-                }
-            }
-        }
-        .task {
-            await fetchData()
-        }
-    }
-
-    func fetchData() async {
-        if isLoading {
-            return
-        }
-
-        isLoading = true
-
-        weeks = Array(repeating: [String](), count: 7)
-
-        do {
-            let weeks_ = try await Request.shared.getGraph(userName: userName)
-            let displayedWeeks = weeks_[weeks_.count - numOfColumns ..< weeks_.count]
-            for week in displayedWeeks {
-                for day in week.contributionDays {
-                    weeks[day.weekday].append(day.contributionLevel)
-                }
-            }
-        } catch {
-            print(error.localizedDescription)
-        }
-
-        isLoading = false
+        .frame(width: graphViewModel.graphFrame.width, height: graphViewModel.graphFrame.height)
     }
 }
 
 struct GraphView_Previews: PreviewProvider {
     static var previews: some View {
-        GraphView(userName: "hikaruaohara", numOfColumns: 16)
+        GraphView(graphViewModel: GraphViewModel(userName: "hikaruaohara"))
     }
 }
--- WristGraph/WristGraph/WristGraphApp.swift
+++ WristGraph/WristGraph/WristGraphApp.swift
@@ -12,6 +12,7 @@ struct WristGraphApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(Model())
         }
     }
 }
